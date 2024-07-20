dfce9b45fee047f044e84b534a585ae76c2b0595
dena <38652032+dena99@users.noreply.github.com>
2024-07-21 03:27:27 +0330

    Update for login and register

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"todo-app/internal/task"
-
 	"fmt"
 	"log"
 	"net/http"
+	"todo-app/internal/task"
+	"todo-app/internal/user"
 
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
@@ -20,6 +20,7 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
+	db.AutoMigrate(&user.User{})
 	db.AutoMigrate(&task.Task{})
 	go task.CheckExpiredTasks(db)
 	router := mux.NewRouter()
@@ -29,6 +30,16 @@ func main() {
 }
 
 func setupRouter(db *gorm.DB, router *mux.Router) *mux.Router {
+	router.HandleFunc("/api/users/register/", func(w http.ResponseWriter, r *http.Request) {
+		user.Register(db, w, r)
+	}).Methods("POST")
+	router.HandleFunc("/api/users/login/", func(w http.ResponseWriter, r *http.Request) {
+		user.Login(db, w, r)
+	}).Methods("POST")
+	router.HandleFunc("/api/users/", func(w http.ResponseWriter, r *http.Request) {
+		user.GetUsers(db, w, r)
+	}).Methods("GET")
+
 	router.HandleFunc("/api/tasks/", func(w http.ResponseWriter, r *http.Request) {
 		task.GetTasks(db, w, r)
 	}).Methods("GET")
